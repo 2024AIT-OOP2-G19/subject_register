e383bc852a796e43701d70f9461863908168f001
ogasa897 <135966582+ogasa897@users.noreply.github.com>
2024-12-12 16:41:38 +0900

    update: routingが正しくなるようにした

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,10 +1,12 @@
 from .user import user_bp
 from .subject import subject_bp
 from .regist import regist_bp
+from .api import api_bp
 
 # Blueprintをリストとしてまとめる
 blueprints = [
   user_bp,
   subject_bp,
-  regist_bp
+  regist_bp,
+  api_bp
 ]
