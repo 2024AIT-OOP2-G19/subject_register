a222c659df7a0cc286b973cb0d361bbd8e3da734
ogasa897 <135966582+ogasa897@users.noreply.github.com>
2024-12-15 15:48:41 +0900

    fix: credit_summary_rankingが正しく動くよう修正
    - credit_summary_rankingがエラーが発生していたのを、正しくAPIが送れるように修正した
    - index.html内のjsを読み込むコードをbodyの最下部に移動した
      -  理由: head内にjsを読み込むコードが書かれていたことで、htmlが読み込まれる前にjsが読み込まれ、グラフが描画されていなかったため
      - 結果: グラフが正しく描画されるようになった

--- routes/api.py
+++ routes/api.py
@@ -47,12 +47,12 @@ api_bp = Blueprint('api', __name__, url_prefix='/api')
 def credit_summary_bar():
     query = (
         Regist.select(
-            User.name, 
-            fn.SUM(Subject.price).alias('total_credits')
+            User.name, #生徒名
+            fn.SUM(Subject.price).alias('total_credits') # 生徒ごとに合計単位数
         )
-        .join(User, on=(Regist.user == User.id))
-        .join(Subject, on=(Regist.subject == Subject.id))
-        .group_by(Regist.user)
+        .join(User, on=(Regist.user == User.id))  # 明示的にUserとの結合条件を指定
+        .join(Subject, on=(Regist.subject == Subject.id))  # 明示的にSubjectとの結合条件を指定
+        .group_by(Regist.user) # 生徒ごとにグループ化
         .order_by(fn.SUM(Subject.price).desc())  # 合計単位数が多い順にソート
     )
     
@@ -68,23 +68,25 @@ def credit_summary_bar():
 # 返す型はjson
 # 生徒ごとに履修合計単位数を返す
 # key=生徒名, value=その生徒の合計単位数 但し、TOP5のみ返す
-# @api_bp.route('/credit_summary_ranking', methods=['GET', 'POST'])
-# def credit_summary_ranking():
-#     query = (
-#         User.select(
-#             User.name, fn.SUM(Subject.price).alias('student_data')
-#         )
-#         .join(Regist, on=(User.name == Regist.user))
-#         .join(Subject, on=(Regist.subject == Subject.name))
-#         .group_by(User.name)
-#         .order_by(fn.SUM(Subject.price).desc())
-#     )
+@api_bp.route('/credit_summary_ranking', methods=['GET', 'POST'])
+def credit_summary_ranking():
+    # queryは上のcredit_summary_barと同じ
+    query = (
+        Regist.select(
+            User.name, 
+            fn.SUM(Subject.price).alias('total_credits')
+        )
+        .join(User, on=(Regist.user == User.id))
+        .join(Subject, on=(Regist.subject == Subject.id))
+        .group_by(Regist.user)
+        .order_by(fn.SUM(Subject.price).desc())
+    )
 
-#     # 上位5名の結果を表示
-#     students_data = {user.name: user.total_credits for user in  enumerate(query.limit(5), start=1)}
+    # 上位5名の結果を表示
+    students_data = { f'rank {rank}:{result.user.name}' : result.total_credits for rank, result in enumerate(query.limit(5), start=1)}
     
-#     result = {
-#         'labels': list(students_data.keys()),
-#         'data': list(students_data.values())
-#     }
-#     return jsonify(result)
+    result = {
+        'labels': list(students_data.keys()),
+        'data': list(students_data.values())
+    }
+    return jsonify(result)
